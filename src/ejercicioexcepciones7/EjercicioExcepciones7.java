86bdfb3bc325318d460662fea58f5bf734532dd9
Piruxb17 <Piruxb17@192.168.1.45>
2023-08-27 20:28:25 -0300

    Haciendo ejercicio 78

--- src/ejercicioexcepciones7/EjercicioExcepciones7.java
+++ src/ejercicioexcepciones7/EjercicioExcepciones7.java
@@ -15,6 +15,7 @@ public class EjercicioExcepciones7 {
      */
     public static void main(String[] args) {
         System.out.println("probando git");
+        System.out.println("probando git");
         // TODO code application logic here
     }
     
