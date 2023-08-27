441f781a942634729230c7ac0410c4dcfd655178
Piruxb17 <Piruxb17@192.168.1.45>
2023-08-27 20:23:50 -0300

    Haciendo ejercicio 7

---
+++ src/ejercicioexcepciones7/EjercicioExcepciones7.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicioexcepciones7;
+
+/**
+ *
+ * @author Piruxb17
+ */
+public class EjercicioExcepciones7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("probando git");
+        // TODO code application logic here
+    }
+    
+}
